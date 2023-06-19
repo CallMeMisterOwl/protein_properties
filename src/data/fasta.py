231e7803691a4fe76886625209c048e3b4b5b55d
CallMeMisterOwl <denizhasler@outlook.de>
2023-06-19 19:37:45 +0200

    Fixed a small bug Had a typo -> test set was actually the train set

--- src/data/fasta.py
+++ src/data/fasta.py
@@ -9,13 +9,13 @@ class Fasta:
 
     Attributes
     ----------
-    _sequences : dict
+    sequences : dict
         Dictionary with the sequences. The keys are the headers and the values are the sequences.
 
     """
 
     def __init__(self, path: Optional[str] = None, sequences: Optional[dict] = None):
-        self._sequences = {} if sequences is None else sequences
+        self.sequences = {} if sequences is None else sequences
         if path is not None:
             self.read_fasta(path)
 
@@ -37,7 +37,7 @@ class Fasta:
             line = line.strip()
             if line.startswith('>'):
                 if header is not None:
-                    self._sequences[header] = sequence
+                    self.sequences[header] = sequence
                     sequence = []
                 if not line[1:]:
                     raise ValueError(f'Fasta file {path} contains an empty header at line {idx}')
@@ -47,7 +47,7 @@ class Fasta:
                 sequence.append([iorf(d) for d in line.split('|')])
             else:
                 sequence.append(line)
-        self._sequences[header] = sequence
+        self.sequences[header] = sequence
 
     def write_fasta(self, path: str, overwrite: bool = False):
         """
@@ -61,7 +61,7 @@ class Fasta:
             raise FileExistsError(f'File {path} already exists')
         path.parents[0].mkdir(parents=True, exist_ok=True)
         with open(path, 'w') as f:
-            for header, sequence in self._sequences.items():
+            for header, sequence in self.sequences.items():
                 f.write(f'>{header}\n')
                 f.write('\n'.join([i if isinstance(i, str) else '|'.join([str(x) for x in i]) for i in sequence]) + '\n')
 
@@ -71,12 +71,12 @@ class Fasta:
         :param header:
         :return: Optional[str]
         """
-        if header in self._sequences:
-            return self._sequences[header]
+        if header in self.sequences:
+            return self.sequences[header]
         else:
             return None
 
-    def get_sequences(self, headers: list) -> Optional[list]:
+    def getsequences(self, headers: list) -> Optional[list]:
         """
         Returns the sequences of a given list of headers.
         :param headers: list
@@ -84,8 +84,8 @@ class Fasta:
         """
         sequences = []
         for header in headers:
-            if header in self._sequences:
-                sequences += [self._sequences[header]]
+            if header in self.sequences:
+                sequences += [self.sequences[header]]
             else:
                 sequences += [None]
         return sequences
@@ -95,14 +95,14 @@ class Fasta:
         Returns the headers of the sequences.
         :return: list
         """
-        return list(self._sequences.keys())
+        return list(self.sequences.keys())
 
-    def get_number_of_sequences_per_header(self) -> int:
+    def get_number_ofsequences_per_header(self) -> int:
         """
         Returns the number of sequences per header i.e. how many lines per header exist.
         :return: int
         """
-        return len(list(self._sequences.values())[0])
+        return len(list(self.sequences.values())[0])
 
     # combine the function names to get_sequence_lengths into one function
 
@@ -117,8 +117,8 @@ class Fasta:
 
         lengths = {}
         for header in headers:
-            if header in self._sequences:
-                lengths[header] = [len(i) for i in self._sequences[header]]
+            if header in self.sequences:
+                lengths[header] = [len(i) for i in self.sequences[header]]
             else:
                 raise KeyError(f'Header {header} not in dictionary')
         return lengths
@@ -128,7 +128,7 @@ class Fasta:
         Returns the lengths of the sequences.
         :return:
         """
-        return {header: [len(i) for i in sequence] for header, sequence in self._sequences.items()}
+        return {header: [len(i) for i in sequence] for header, sequence in self.sequences.items()}
 
     def append(self, other: Union['Fasta', dict]):
         """
@@ -138,49 +138,49 @@ class Fasta:
         :return:
         """
         if isinstance(other, Fasta):
-            for key, value in other._sequences.items():
+            for key, value in other.sequences.items():
                 if isinstance(value, list):
-                    self._sequences.setdefault(key, []).extend(value)
+                    self.sequences.setdefault(key, []).extend(value)
                 elif isinstance(value, str):
-                    self._sequences.setdefault(key, []).append(value)
+                    self.sequences.setdefault(key, []).append(value)
         elif isinstance(other, dict):
             for key, value in other.items():
                 if isinstance(value, list):
-                    self._sequences.setdefault(key, []).extend(value)
+                    self.sequences.setdefault(key, []).extend(value)
                 elif isinstance(value, str):
-                    self._sequences.setdefault(key, []).append(value)
+                    self.sequences.setdefault(key, []).append(value)
         return self
 
     def __copy__(self):
-        return Fasta(sequences=self._sequences)
+        return Fasta(sequences=self.sequences)
 
     def __repr__(self):
-        return f'Fasta file with {len(self._sequences)} sequences'
+        return f'Fasta file with {len(self.sequences)} sequences'
 
     def __str__(self):
-        return f'Fasta file with {len(self._sequences)} sequences'
+        return f'Fasta file with {len(self.sequences)} sequences'
 
     def __len__(self):
-        return len(self._sequences)
+        return len(self.sequences)
 
     def __getitem__(self, key):
-        return self._sequences[key]
+        return self.sequences[key]
 
     def __setitem__(self, key, value):
-        self._sequences[key] = value
+        self.sequences[key] = value
 
     def __delitem__(self, key):
-        del self._sequences[key]
+        del self.sequences[key]
 
     def __iter__(self):
-        return iter(self._sequences)
+        return iter(self.sequences)
 
     def __contains__(self, key):
-        return key in self._sequences
+        return key in self.sequences
 
     def __add__(self, other):
         if isinstance(other, Fasta):
-            self._sequences.update(other._sequences)
+            self.sequences.update(other.sequences)
         else:
             raise TypeError(f'Cannot add Fasta and {type(other)}')
         return self
