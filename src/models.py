467dde672dc4c67f2074cbdb395568d0eb4761e5
CallMeMisterOwl <denizhasler@outlook.de>
2023-07-06 15:51:42 +0200

    Update Baseline

--- src/models.py
+++ src/models.py
@@ -1,29 +1,35 @@
 from dataclasses import dataclass
 from typing import Any, Literal
-import pytorch_lightning as pl
+import lightning.pytorch as pl
 import torch.nn.functional as F
 import torch
 from torch import nn
-from torchmetrics.functional.classification import binary_f1_score, multiclass_f1_score
+from torchmetrics.functional.classification import binary_f1_score, multiclass_f1_score, binary_accuracy, multiclass_accuracy
+from typing import Optional
     
 
 class SASABaseline(pl.LightningModule):
     def __init__(self, 
-                 num_classes: Literal[2,3,10] = 3,
-                 class_weights: Any = None,
+                 num_classes: Literal[2, 3, 10] = 3,
+                 class_weights: torch.Tensor = None,
                  lr: float = 1e-3,
                  weight_decay: float = 0.0,
-                 loss_fn: Any = None,
                  **kwargs):
         super().__init__()
         self.num_classes = num_classes
         self.class_weights = class_weights
         self.lr = lr
         self.weight_decay = weight_decay
-        self.loss_fn = loss_fn
+        self.loss_fn = None
         self.model = nn.Sequential(
             nn.Linear(1024, self.num_classes if self.num_classes > 2 else 1),
         )
+        self.lr_scheduler = kwargs.get("lr_scheduler", None)
+        self.output_path = kwargs.get("output_path", ".")
+
+        self.hparams["Modeltype"] = "SASABaseline"
+        self.save_hyperparameters("num_classes", "class_weights", "lr", "weight_decay")
+        
 
     def forward(self, x):
         return self.model(x)
@@ -35,13 +41,10 @@ class SASABaseline(pl.LightningModule):
         mask = (y != -1)
         
         loss = self._loss(y_hat[mask], y[mask])
-        self.log("train_loss", loss, on_step=True, on_epoch=True)
+        self.log("train_loss", loss, on_step=False, on_epoch=True)
         for t in self._accuracy(y_hat[mask], y[mask]):
-            self.log(f"train_{t[0]}", t[1], on_epoch=True)
+            self.log(f"train_{t[0]}", t[1], on_epoch=True, on_step=False)
         return loss
-    
-    def on_train_epoch_end(self) -> None:
-        return super().on_train_epoch_end()
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
@@ -50,9 +53,9 @@ class SASABaseline(pl.LightningModule):
         mask = (y != -1)
     
         loss = self._loss(y_hat[mask], y[mask])
-        self.log("val_loss", loss)
+        self.log("val_loss", loss, on_step=False, on_epoch=True)
         for t in self._accuracy(y_hat[mask], y[mask]):
-            self.log(f"val_{t[0]}", t[1], on_epoch=True)
+            self.log(f"val_{t[0]}", t[1], on_epoch=True, on_step=False)
         return loss
     
     def test_step(self, batch, batch_idx):
@@ -61,9 +64,9 @@ class SASABaseline(pl.LightningModule):
         y_hat = self(x).squeeze()        
         mask = (y != -1)
         loss = self._loss(y_hat[mask], y[mask])
-        self.log("test_loss", loss)
+        self.log("test_loss", loss, on_step=False, on_epoch=True)
         for t in self._accuracy(y_hat[mask], y[mask]):
-            self.log(f"test_{t[0]}", t[1], on_epoch=True)
+            self.log(f"test_{t[0]}", t[1], on_epoch=True, on_step=False)
         return loss
     
     def _configure_optimizer(self, optim_config = None):
@@ -85,13 +88,15 @@ class SASABaseline(pl.LightningModule):
     def _accuracy(self, y_hat, y):
         metrics = []
         if self.num_classes == 2:
-            return [("F1", binary_f1_score(y_hat, y))]
-        return [("F1", multiclass_f1_score(y_hat, y, num_classes=self.num_classes))]
+            return [("Accuracy", binary_accuracy(y_hat, y))]
+        return [("Accuracy", multiclass_accuracy(y_hat, y, num_classes=self.num_classes))]
 
     def _loss(self, y_hat, y):
         if self.loss_fn is not None:
             return self.loss_fn(y_hat, y, self.class_weights)
-        if self.num_classes == 2:
+        if self.num_classes == 1:
+            return F.mse_loss(y_hat, y)
+        elif self.num_classes == 2:
             return F.binary_cross_entropy_with_logits(y_hat, y, pos_weight=self.class_weights)
         return F.cross_entropy(y_hat, y, weight=self.class_weights)
     
